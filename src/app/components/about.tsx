6ef8dedb6365f4d88d4306cda5374c2583bb1c42
junbeomwooo <junbeom2.woo@gmail.com>
2024-12-20 20:13:35 +0100

    implement Skills section

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -5,7 +5,7 @@ import { ReactTyped } from "react-typed";
 import { useLocale } from "next-intl";
 
 // Import Swiper React components
-import { Swiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -13,6 +13,20 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Mousewheel, Pagination } from "swiper/modules";
+
+// Icons
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaJs } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { DiMysql } from "react-icons/di";
+import { SiAdobeillustrator } from "react-icons/si";
+import { SiAdobephotoshop } from "react-icons/si";
 
 export default function About() {
   const locale = useLocale();
@@ -22,16 +36,17 @@ export default function About() {
       <Swiper
         direction={"vertical"}
         slidesPerView={1}
-        spaceBetween={30}
+        spaceBetween={0}
         mousewheel={true}
         pagination={{
           clickable: true,
         }}
-        loop={true}  
+        loop={false}
         modules={[Mousewheel, Pagination]}
-        className="w-full h-full"
+        className="w-full h-screen"
       >
         {/* First: Who am I  */}
+        <SwiperSlide className="w-full h-screen">
           <div className="w-full h-screen pt-[135px] md:pt-[220px] xl:pt-0 block xl:flex px-6 md:px-10 lg:px-40 justify-between items-center">
             {/* photo */}
             <Image
@@ -49,7 +64,7 @@ export default function About() {
                   function
                 </h2>
                 <h1 className="text-[20px] sm:text-[28px] md:text-[30px] xl:text-[40px] text-orange-500 dark:text-[#88ff69] font-monaco font-bold mt-10">
-                  About( ) {"{"}
+                  WhoAmI( ) {"{"}
                 </h1>
               </div>
 
@@ -93,11 +108,374 @@ export default function About() {
               className="hidden m-auto xl:block xl:w-[400px] xl:h-[400px]"
             />
           </div>
+        </SwiperSlide>
 
-        {/* second */}
-        {/* <SwiperSlide className="h-screen">
-          <div className="w-full h-screen" >NICE</div>
-        </SwiperSlide> */}
+        {/* Second: Skills */}
+        <SwiperSlide className="w-full h-screen">
+          <div className="w-full h-screen pt-[135px] md:pt-[220px] xl:pt-0 block xl:flex px-6 md:px-10 lg:px-40 justify-between items-center">
+            {/* Skills */}
+            <div className="w-full xl:w-1/2 mt-14 sm:mt-0">
+              {/* funtion Skills() */}
+              <div className="flex items-end">
+                <h2 className="text-[20px] md:text-[30px] xl:text-[40px] mr-4 text-[#8c1490] font-monaco font-bold ">
+                  function
+                </h2>
+                <h1 className="text-[20px] sm:text-[28px] md:text-[30px] xl:text-[40px] text-orange-500 dark:text-[#88ff69] font-monaco font-bold mt-10">
+                  Skills( ) {"{"}
+                </h1>
+              </div>
+
+              {/* text */}
+              <div className="py-4 xl:max-w-[900px] hidden md:block">
+                <ReactTyped
+                  strings={
+                    locale === "en"
+                      ? [
+                          "I specialize in front-end development.<br />" +
+                            "I also have experience in full-stack development using Node.js, Express and MySQL.<br /> <br />" +
+                            " I primarily build web applications using HTML, CSS, and JS, with React and Next.js.<br />" +
+                            "    I handle data through server and API communication, efficiently processing it and displaying it in a user-friendly design.",
+                        ]
+                      : [
+                          "Jeg specialiserer mig i front-end udvikling, men har også erfaring med full-stack udvikling ved hjælp af Node.js, Express og MySQL.<br /> <br />" +
+                            "Jeg bygger primært webapplikationer ved hjælp af HTML, CSS og JS, med React og Next.js.<br />" +
+                            "Jeg håndterer data gennem server- og API-kommunikation, behandler dem effektivt og viser dem i et brugervenligt design.",
+                        ]
+                  }
+                  typeSpeed={3}
+                  className="text-[13px] sm:text-[15px] md:text-[18px] xl:text-[20px] leading-[20px] sm:leading-[24px] xl:leading-[34px] overflow-y-auto"
+                  loop={false}
+                  showCursor={true}
+                  contentType="html"
+                />
+              </div>
+
+              {/* graph */}
+              <div className="font-medium text-sm xl:hidden ">
+                {/* HTML & CSS */}
+                <div>
+                  <div className="flex justify-between mt-6">
+                    <h3>HTML & CSS</h3>
+                    <h3 className="text-sm font-normal">100</h3>
+                  </div>
+                  <div className="w-[100%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+                </div>
+
+                {/* JS & TS */}
+                <div>
+                  <div className="flex justify-between mt-4">
+                    <h3>JAVASCRIPT & TYPESCRIPT</h3>
+                    <h3 className="text-sm font-normal">80</h3>
+                  </div>
+                  <div className="w-[80%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+                </div>
+                {/* React & Next.js */}
+                <div>
+                  <div className="flex justify-between mt-4">
+                    <h3>REACT & NEXT.JS</h3>
+                    <h3 className="text-sm font-normal">90</h3>
+                  </div>
+                  <div className="w-[90%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+                </div>
+
+                {/* Node.js & EXPRESS */}
+                <div>
+                  <div className="flex justify-between mt-4">
+                    <h3>NODE.JS & EXPRESS</h3>
+                    <h3 className="text-sm font-normal">70</h3>
+                  </div>
+                  <div className="w-[70%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+                </div>
+
+                {/* MySQL*/}
+                <div>
+                  <div className="flex justify-between mt-4">
+                    <h3>MYSQL</h3>
+                    <h3 className="text-sm font-normal">70</h3>
+                  </div>
+                  <div className="w-[70%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+                </div>
+
+                {/* ADOBE ILLUSTRATOR & PHOTOSHOP*/}
+                <div>
+                  <div className="flex justify-between mt-4">
+                    <h3>Adobe Illustrator & PhotoShop</h3>
+                    <h3 className="text-sm font-normal">80</h3>
+                  </div>
+                  <div className="w-[80%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+                </div>
+
+                {/* hover me! */}
+                <div className="mt-10 hidden sm:block">
+                  {" "}
+                  <h3 className="font-medium text-orange-500 dark:text-[#88ff69]">Hover Me !</h3>
+                </div>
+                {/* skills icon */}
+                <div className="mt-4 gap-4 flex-wrap hidden sm:flex  opacity-60">
+                  {/* html */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="HTML"
+                  >
+                    <FaHtml5 className="w-[25px] h-[25px] text-white dark:text-black" />
+                  </button>
+
+                  {/* css */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="CSS"
+                  >
+                    <FaCss3Alt className="w-[25px] h-[25px] text-white dark:text-black" />
+                  </button>
+
+                  {/* TailwindCss */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="TAILWIND"
+                  >
+                    <RiTailwindCssFill className="w-[25px] h-[25px] text-white dark:text-black" />
+                  </button>
+
+                  {/* JS */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="JS"
+                  >
+                    <FaJs className="w-[20px] h-[20px] text-white dark:text-black" />
+                  </button>
+
+                  {/* TS */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="TS"
+                  >
+                    <SiTypescript className="w-[20px] h-[20px] text-white dark:text-black" />
+                  </button>
+
+                  {/* React */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="REACT"
+                  >
+                    <FaReact className="w-[25px] h-[25px] text-white dark:text-black" />
+                  </button>
+
+                  {/* Next.js */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="NEXT"
+                  >
+                    <RiNextjsFill className="w-[25px] h-[25px] text-white dark:text-black" />
+                  </button>
+
+                  {/* Node.js */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="NODE"
+                  >
+                    <FaNodeJs className="w-[25px] h-[25px] text-white dark:text-black" />
+                  </button>
+
+                  {/* Express */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="EXPRESS"
+                  >
+                    <SiExpress className="w-[25px] h-[25px] text-white dark:text-black" />
+                  </button>
+
+                  {/* MySQL */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="MYSQL"
+                  >
+                    <DiMysql className="w-[25px] h-[25px] text-white dark:text-black" />
+                  </button>
+
+                  {/* Illustrator */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="ILLUSTRATOR"
+                  >
+                    <SiAdobeillustrator className="w-[20px] h-[20px] text-white dark:text-black" />
+                  </button>
+
+                  {/* PhotoShop */}
+                  <button
+                    className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                    data-name="PHOTOSHOP"
+                  >
+                    <SiAdobephotoshop className="w-[20px] h-[20px] text-white dark:text-black" />
+                  </button>
+                </div>
+              </div>
+              <h1 className="text-[20px] sm:text-[28px] md:text-[30px] text-orange-500 dark:text-[#88ff69] font-monaco font-bold xl:text-[40px] mt-6">
+                {"}"}
+              </h1>
+            </div>
+            {/* graph 2 */}
+            <div className="font-medium hidden text-[18px] xl:block xl:w-1/2 ml-20 ">
+              {/* HTML & CSS */}
+              <div>
+                <div className="flex justify-between ">
+                  <h3>HTML & CSS</h3>
+                  <h3 className="text-sm font-normal">100</h3>
+                </div>
+                <div className="w-[100%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+              </div>
+
+              {/* JS & TS */}
+              <div>
+                <div className="flex justify-between mt-8">
+                  <h3>JAVASCRIPT & TYPESCRIPT</h3>
+                  <h3 className="text-sm font-normal">80</h3>
+                </div>
+                <div className="w-[80%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+              </div>
+
+              {/* React & Next.js */}
+              <div>
+                <div className="flex justify-between mt-8">
+                  <h3>REACT & NEXT.JS</h3>
+                  <h3 className="text-sm font-normal">90</h3>
+                </div>
+                <div className="w-[90%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+              </div>
+
+              {/* Node.js & EXPRESS */}
+              <div>
+                <div className="flex justify-between mt-8">
+                  <h3>NODE.JS & EXPRESS</h3>
+                  <h3 className="text-sm font-normal">70</h3>
+                </div>
+                <div className="w-[70%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+              </div>
+
+              {/* MySQL*/}
+              <div>
+                <div className="flex justify-between mt-8">
+                  <h3>MYSQL</h3>
+                  <h3 className="text-sm font-normal">70</h3>
+                </div>
+                <div className="w-[70%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+              </div>
+
+              {/* ADOBE ILLUSTRATOR & PHOTOSHOP*/}
+              <div>
+                <div className="flex justify-between mt-8">
+                  <h3>Adobe Illustrator & Photoshop</h3>
+                  <h3 className="text-sm font-normal">80</h3>
+                </div>
+                <div className="w-[80%] h-[5px] bg-orange-500 mt-2 dark:bg-[#88ff69]" />
+              </div>
+
+                  {/* hover me! */}
+              <div className="mt-10 hidden sm:block">
+                  {" "}
+                  <h3 className="font-medium text-[15px] text-orange-500 dark:text-[#88ff69]">Hover Me !</h3>
+                </div>
+
+              {/* skills icon */}
+              <div className="mt-4 gap-4 flex-wrap hidden sm:flex  opacity-60">
+                {/* html */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="HTML"
+                >
+                  <FaHtml5 className="w-[25px] h-[25px] text-white dark:text-black" />
+                </button>
+
+                {/* css */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="CSS"
+                >
+                  <FaCss3Alt className="w-[25px] h-[25px] text-white dark:text-black" />
+                </button>
+
+                {/* TailwindCss */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="TAILWIND"
+                >
+                  <RiTailwindCssFill className="w-[25px] h-[25px] text-white dark:text-black" />
+                </button>
+
+                {/* JS */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="JS"
+                >
+                  <FaJs className="w-[20px] h-[20px] text-white dark:text-black" />
+                </button>
+
+                {/* TS */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="TS"
+                >
+                  <SiTypescript className="w-[20px] h-[20px] text-white dark:text-black" />
+                </button>
+
+                {/* React */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="REACT"
+                >
+                  <FaReact className="w-[25px] h-[25px] text-white dark:text-black" />
+                </button>
+
+                {/* Next.js */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="NEXT"
+                >
+                  <RiNextjsFill className="w-[25px] h-[25px] text-white dark:text-black" />
+                </button>
+
+                {/* Node.js */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="NODE"
+                >
+                  <FaNodeJs className="w-[25px] h-[25px] text-white dark:text-black" />
+                </button>
+
+                {/* Express */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="EXPRESS"
+                >
+                  <SiExpress className="w-[25px] h-[25px] text-white dark:text-black" />
+                </button>
+
+                {/* MySQL */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="MYSQL"
+                >
+                  <DiMysql className="w-[25px] h-[25px] text-white dark:text-black" />
+                </button>
+
+                {/* Illustrator */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="ILLUSTRATOR"
+                >
+                  <SiAdobeillustrator className="w-[20px] h-[20px] text-white dark:text-black" />
+                </button>
+
+                {/* PhotoShop */}
+                <button
+                  className="bg-orange-500 w-[40px] h-[40px] rounded-full flex items-center justify-center dark:bg-[#88ff69] hoverable2"
+                  data-name="PHOTOSHOP"
+                >
+                  <SiAdobephotoshop className="w-[20px] h-[20px] text-white dark:text-black" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
--- src/app/components/customCursor.tsx
+++ src/app/components/customCursor.tsx
@@ -2,11 +2,38 @@
 
 import { useEffect, useRef, useState } from "react";
 
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaJs } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { DiMysql } from "react-icons/di";
+import { SiAdobeillustrator } from "react-icons/si";
+import { SiAdobephotoshop } from "react-icons/si";
+
 export default function CustomCursor() {
+  /** for hovering */
+
   // 커서 참조
   const cursorRef = useRef<HTMLDivElement | null>(null);
+
   // hover 상태 추적
   const [isHovering, setIsHovering] = useState(false);
+
+  /** for magnifying */
+
+  // hover 상태 추적
+  const [isMagnifying, setIsMagnifying] = useState(false);
+
+  // 커서에 표시할 텍스트
+  const [cursorText, setCursorText] = useState<string>("");
+
+  // 커서에 표시할 아이콘
+  const [cursorIcon, setCursorIcon] = useState<JSX.Element | null>(null);
 
   useEffect(() => {
     if (cursorRef.current == null) return;
@@ -23,23 +50,96 @@ export default function CustomCursor() {
     // `mouseenter`와 `mouseleave`로 hover 상태 추적
     const handleHover = (e: MouseEvent) => {
       if (e.target instanceof HTMLElement) {
+        // 단순 호버 전용
         if (e.target.matches(".hoverable")) {
-          setIsHovering(true); // 해당 요소에 마우스가 올라오면 hover 상태로 설정
+          setIsHovering(true);
+
+          // 호버 시 동시에 확대 + 텍스트 까지해주기위한
+        } else if (e.target.matches(".hoverable2")) {
+          setIsMagnifying(true);
+          setCursorText(e.target.getAttribute("data-name") || "");
+
+          // 아이콘 설정
+          switch (e.target.getAttribute("data-name")) {
+            case "HTML":
+              setCursorIcon(
+                <FaHtml5 className="text-white dark:text-black w-[10px] h-[10px]" />
+              );
+              break;
+            case "CSS":
+              setCursorIcon(
+                <FaCss3Alt className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "TAILWIND":
+              setCursorIcon(
+                <RiTailwindCssFill className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "JS":
+              setCursorIcon(<FaJs className="text-white dark:text-black  w-[10px] h-[10px]" />);
+              break;
+            case "TS":
+              setCursorIcon(
+                <SiTypescript className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "REACT":
+              setCursorIcon(
+                <FaReact className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "NEXT":
+              setCursorIcon(
+                <RiNextjsFill className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "NODE":
+              setCursorIcon(
+                <FaNodeJs className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "EXPRESS":
+              setCursorIcon(
+                <SiExpress className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "MYSQL":
+              setCursorIcon(
+                <DiMysql className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "ILLUSTRATOR":
+              setCursorIcon(
+                <SiAdobeillustrator className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            case "PHOTOSHOP":
+              setCursorIcon(
+                <SiAdobephotoshop className="text-white dark:text-black  w-[10px] h-[10px]" />
+              );
+              break;
+            default:
+              setCursorIcon(null); // 기본값은 아이콘을 비우거나 처리
+              break;
+          }
         } else {
-          setIsHovering(false); // 아니면 hover 상태 해제
+          setIsHovering(false);
+          setIsMagnifying(false);
+          setCursorIcon(null);
+          setCursorText("");
         }
       }
     };
 
-
     const handleClick = () => {
+      if (cursorRef.current == null) return;
+      cursorRef.current.classList.add("expand");
+      setTimeout(() => {
         if (cursorRef.current == null) return;
-        cursorRef.current.classList.add("expand");
-        setTimeout(() => {
-          if (cursorRef.current == null) return;
-          cursorRef.current.classList.remove("expand");
-        }, 500);
-    }
+        cursorRef.current.classList.remove("expand");
+      }, 500);
+    };
     document.addEventListener("mouseover", handleHover);
     document.addEventListener("mouseout", handleHover);
     document.addEventListener("click", handleClick);
@@ -54,8 +154,19 @@ export default function CustomCursor() {
 
   return (
     <div
-      className={`cursor ${isHovering ? "hovering" : ""}`}
+      className={`cursor ${
+        isHovering ? "hovering" : isMagnifying ? "magnifying" : ""
+      }`}
       ref={cursorRef}
-    ></div>
+    >
+      <div>
+        {/* 아이콘 */}
+        <div className="flex justify-center">{cursorIcon}</div>
+        {/* 텍스트 */}
+        <h3 className="cursor-text text-white text-[2px] text-center mt-[2px] dark:text-black">
+          {cursorText}
+        </h3>
+      </div>
+    </div>
   );
 }
