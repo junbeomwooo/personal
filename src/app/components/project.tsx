b0cf4d5977771dddd5d69cfc47c9d8b8c73035cc
junbeomwooo <junbeom2.woo@gmail.com>
2024-12-30 00:02:53 +0100

    30 Dec 2024

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -13,15 +13,16 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 
 export default function Project() {
-
   const [title, setTitle] = useState("FOTBOLD");
+
+  console.log(window.innerWidth)
 
   return (
     <AnimatePresence>
       {/* Container */}
-      <div className="w-full h-screen pt-[135px] md:pt-[30px] xl:pt-0 flex px-6 md:px-10 lg:px-20 xl:items-center">
+      <div className="w-full h-screen flex px-6 md:px-10 lg:px-40 xl:px-20 2xl:px-40 items-center pt-[45px]">
         {/* Box */}
-        <div className="w-full">
+        <div className="w-full sm:scale-90 md:scale-[85%] xl:scale-100">
           <motion.div
             className="flex items-end"
             initial={{ opacity: 0, y: "-35%" }}
@@ -43,122 +44,85 @@ export default function Project() {
               swiper.navigation.nextEl.classList.add("hoverable");
               swiper.navigation.prevEl.classList.add("hoverable");
             }}
+            className="relative"
             onSlideChange={(e) => {
               if (e.activeIndex === 0) {
-                setTitle("FOTBOLD")
+                setTitle("FOTBOLD");
               } else if (e.activeIndex === 1) {
-                setTitle("NMP")
+                setTitle("NMP");
               } else if (e.activeIndex === 2) {
-                setTitle("UNICO")
+                setTitle("UNICO");
               }
             }}
           >
             {/* fotbold */}
             <SwiperSlide>
-              <div className="w-full block xl:flex justify-between gap-[50px] py-6 px-20 ">
+              <div className="w-full block xl:flex justify-between gap-[60px] py-4 xl:py-14 px-12 sm:px-20 items-center">
                 {/* photo */}
                 <Image
-                  src="/Fotbold.png"
+                  src="/fotbold_mockup.jpg"
                   alt="Fotbold : Live Football Scores & Comprehensive Football Info"
-                  width={1000}
-                  height={1000}
-                  className="w-full xl:w-1/2 h-auto"
+                  width={3000}
+                  height={2000}
+                  className="w-full xl:w-1/2 h-auto hoverable hover:opacity-80"
                 />
 
                 {/* explantion */}
                 <div className="w-full xl:w-1/2">
-                  <h1 className="text-[14px] sm:text-[17px] md:text-[20px] xl:text-[22px]  font-medium leading-[18px] mt-8 xl:mt-0">
+                  <h1 className="text-[14px] sm:text-[17px] md:text-[20px] xl:text-[22px]  font-semibold leading-[18px] mt-4 sm:mt-6 xl:mt-0 md:leading-[30px]">
                     Fotbold: Live Football Scores & Comprehensive Football Info
                   </h1>
-                  <h2 className="text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] mt-4 xl:mt-5 text-[#888888] dark:text-[#b4b4b4]">
+                  <h2 className="text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] mt-2 sm:mt-4 xl:mt-5 text-[#888888] dark:text-[#b4b4b4]">
                     Jul 2024 - Present
                   </h2>
-                  <h3 className="mt-4 xl:mt-5 text-[11px] sm:text-[14px] md:text-[17px] xl:text-[19px] leading-[25px]">
+                  <h3 className="mt-2 sm:mt-4 xl:mt-5 text-[13px] sm:text-[15px] md:text-[18px] xl:text-[20px] leading-[15px] sm:leading-[30px]">
                     I developed a movie chart site named “NMP” that showcases
                     popular recent movies as part of a mini-project. I used
                     Next.js and TypeScript, and it is deployed on Vercel.
                   </h3>
-                  <h4 className="mt-4 xl:mt-5 text-[11px] sm:text-[14px] md:text-[17px] xl:text-[19px]">
-                    • Client : HTML, CSS, Next.js, TypeScript
-                    <br />
-                    <br />
-                    • Version and issue management : Github <br />
-                    <br />• Deployment environment : Vercel • Design : Adobe
-                    Illustrator, Adobe Photoshop
-                  </h4>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="w-full block xl:flex justify-between gap-[50px] py-6 px-20 ">
-                {/* photo */}
-                <Image
-                  src="/Fotbold.png"
-                  alt="Fotbold : Live Football Scores & Comprehensive Football Info"
-                  width={1000}
-                  height={1000}
-                  className="w-full xl:w-1/2 h-auto"
-                />
 
-                {/* explantion */}
-                <div className="w-full xl:w-1/2">
-                  <h1 className="text-[14px] sm:text-[17px] md:text-[20px] xl:text-[22px]  font-medium leading-[18px] mt-8 xl:mt-0">
-                    Fotbold: Live Football Scores & Comprehensive Football Info
-                  </h1>
-                  <h2 className="text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] mt-4 xl:mt-5 text-[#888888] dark:text-[#b4b4b4]">
-                    Jul 2024 - Present
-                  </h2>
-                  <h3 className="mt-4 xl:mt-5 text-[11px] sm:text-[14px] md:text-[17px] xl:text-[19px] leading-[25px]">
-                    I developed a movie chart site named “NMP” that showcases
-                    popular recent movies as part of a mini-project. I used
-                    Next.js and TypeScript, and it is deployed on Vercel.
-                  </h3>
-                  <h4 className="mt-4 xl:mt-5 text-[11px] sm:text-[14px] md:text-[17px] xl:text-[19px]">
-                    • Client : HTML, CSS, Next.js, TypeScript
-                    <br />
-                    <br />
-                    • Version and issue management : Github <br />
-                    <br />• Deployment environment : Vercel • Design : Adobe
-                    Illustrator, Adobe Photoshop
-                  </h4>
+                  {/* skills */}
+                  <div className="flex gap-2 mt-4 sm:mt-6 flex-wrap">
+                    <h1 className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-400 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      HTML
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      CSS
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400   dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Tailwind CSS
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Next.js
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      TypeScript
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Github
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Vercel
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Adobe Illustrator
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Adobe Photoshop
+                    </h1>
+                  </div>
+                  {/* buttons */}
+                  <div className="flex justify-between mt-6 sm:mt-11 gap-4">
+     
+                    <button className="bg-none w-1/2 py-3 text-orange-500 font-medium border-2 border-solid border-orange-500 hover:no-underline hoverable hover:bg-orange-500 hover:text-white hover:dark:text-white dark:border-green-500 dark:text-green-500 hover:dark:bg-green-500 text-[13px] sm:text-base">Move to page</button>
+                    <button className="bg-none w-1/2 py-3 text-orange-500 font-medium border-2 border-solid border-orange-500 hover:no-underline hoverable hover:bg-orange-500 hover:text-white hover:dark:text-white dark:border-green-500 dark:text-green-500 hover:dark:bg-green-500 text-[13px] sm:text-base">View on Github</button>
+                  </div>
                 </div>
               </div>
             </SwiperSlide>
-            <SwiperSlide>
-              <div className="w-full block xl:flex justify-between gap-[50px] py-6 px-20 ">
-                {/* photo */}
-                <Image
-                  src="/Fotbold.png"
-                  alt="Fotbold : Live Football Scores & Comprehensive Football Info"
-                  width={1000}
-                  height={1000}
-                  className="w-full xl:w-1/2 h-auto"
-                />
 
-                {/* explantion */}
-                <div className="w-full xl:w-1/2">
-                  <h1 className="text-[14px] sm:text-[17px] md:text-[20px] xl:text-[22px]  font-medium leading-[18px] mt-8 xl:mt-0">
-                    Fotbold: Live Football Scores & Comprehensive Football Info
-                  </h1>
-                  <h2 className="text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] mt-4 xl:mt-5 text-[#888888] dark:text-[#b4b4b4]">
-                    Jul 2024 - Present
-                  </h2>
-                  <h3 className="mt-4 xl:mt-5 text-[11px] sm:text-[14px] md:text-[17px] xl:text-[19px] leading-[25px]">
-                    I developed a movie chart site named “NMP” that showcases
-                    popular recent movies as part of a mini-project. I used
-                    Next.js and TypeScript, and it is deployed on Vercel.
-                  </h3>
-                  <h4 className="mt-4 xl:mt-5 text-[11px] sm:text-[14px] md:text-[17px] xl:text-[19px]">
-                    • Client : HTML, CSS, Next.js, TypeScript
-                    <br />
-                    <br />
-                    • Version and issue management : Github <br />
-                    <br />• Deployment environment : Vercel • Design : Adobe
-                    Illustrator, Adobe Photoshop
-                  </h4>
-                </div>
-              </div>
-            </SwiperSlide>
+            {/* unico */}
+
           </Swiper>
 
           <motion.h1
