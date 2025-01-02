2c30e40daf3a9a3f09903eed35e57388fb037d5d
junbeomwooo <junbeom2.woo@gmail.com>
2025-01-02 17:18:28 +0100

    02 Jan 2025

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -31,6 +31,9 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -119,6 +119,67 @@ export default function Project() {
             </SwiperSlide>
 
             {/* unico */}
+            <SwiperSlide>
+              <div className="w-full block xl:flex justify-between gap-[60px] py-4 xl:py-14 px-12 sm:px-20 items-center">
+                {/* photo */}
+                <Image
+                  src="/fotbold_mockup.jpg"
+                  alt="Fotbold : Live Football Scores & Comprehensive Football Info"
+                  width={3000}
+                  height={2000}
+                  className="w-full xl:w-1/2 h-auto hoverable hover:opacity-80"
+                />
+
+                {/* explantion */}
+                <div className="w-full xl:w-1/2">
+                  <h1 className="text-[14px] sm:text-[17px] md:text-[20px] xl:text-[22px]  font-semibold leading-[18px] mt-4 sm:mt-6 xl:mt-0 md:leading-[30px]">
+                    Fotbold: Live Football Scores & Comprehensive Football Info
+                  </h1>
+                  <h2 className="text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] mt-2 sm:mt-4 xl:mt-5 text-[#888888] dark:text-[#b4b4b4]">
+                    Jul 2024 - Present
+                  </h2>
+                  <h3 className="mt-2 sm:mt-4 xl:mt-5 text-[13px] sm:text-[15px] md:text-[18px] xl:text-[20px] leading-[15px] sm:leading-[30px]">
+                    I developed a movie chart site named “NMP” that showcases
+                    popular recent movies as part of a mini-project. I used
+                    Next.js and TypeScript, and it is deployed on Vercel.
+                  </h3>
+
+                  {/* skills */}
+                  <div className="flex gap-2 mt-4 sm:mt-6 flex-wrap">
+                    <h1 className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-400 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      HTML
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      CSS
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Next.js
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      TypeScript
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Github
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Vercel
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Adobe Illustrator
+                    </h1>
+                    <h1 className="px-1 sm:px-3  py-0.5 sm:py-1 bg-orange-400  dark:bg-green-600  text-[10px] sm:text-sm rounded-lg text-white font-semibold">
+                      Adobe Photoshop
+                    </h1>
+                  </div>
+                  {/* buttons */}
+                  <div className="flex justify-between mt-6 sm:mt-11 gap-4">
+     
+                    <button className="bg-none w-1/2 py-3 text-orange-500 font-medium border-2 border-solid border-orange-500 hover:no-underline hoverable hover:bg-orange-500 hover:text-white hover:dark:text-white dark:border-green-500 dark:text-green-500 hover:dark:bg-green-500 text-[13px] sm:text-base">Move to page</button>
+                    <button className="bg-none w-1/2 py-3 text-orange-500 font-medium border-2 border-solid border-orange-500 hover:no-underline hoverable hover:bg-orange-500 hover:text-white hover:dark:text-white dark:border-green-500 dark:text-green-500 hover:dark:bg-green-500 text-[13px] sm:text-base">View on Github</button>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
 
           </Swiper>
 
