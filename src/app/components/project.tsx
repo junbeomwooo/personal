ec55028ce46932a35fcb7a46ade0c1bdbf3ebc23
junbeomwooo <junbeom2.woo@gmail.com>
2025-09-03 16:00:42 +0200

    Add new project info

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -21,7 +21,7 @@ import { Navigation } from "swiper/modules";
 import ShuffleText from "react-shuffle-text";
 
 export default function Project() {
-  const [title, setTitle] = useState("FOTBOLD");
+  const [title, setTitle] = useState("WEATHER_APP");
 
   const locale = useLocale();
 
@@ -56,14 +56,185 @@ export default function Project() {
             className="absolute h-auto"
             onSlideChange={(e) => {
               if (e.activeIndex === 0) {
-                setTitle("FOTBOLD");
+                setTitle("WEATHER_APP");
               } else if (e.activeIndex === 1) {
-                setTitle("UNICO");
+                setTitle("FOTBOLD");
               } else if (e.activeIndex === 2) {
+                setTitle("UNICO");
+              } else if (e.activeIndex === 3) {
                 setTitle("NMP");
               }
             }}
           >
+            {/* Weather App */}
+            <SwiperSlide>
+              <div className="w-full h-auto block xl:flex justify-between gap-[60px] py-4 xl:py-14 px-10 sm:px-20 items-center">
+                {/* photo */}
+                <motion.div
+                  initial={{ scale: 0 }}
+                  animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                  transition={{ duration: 0.5, ease: "easeInOut" }}
+                  className="w-full xl:w-1/2 h-auto hoverable hover:opacity-80"
+                >
+                  <Image
+                    src={
+                      mode === "light"
+                        ? "/weather_app_mockup.jpeg"
+                        : "/weather_app_mockup_dark.jpeg"
+                    }
+                    alt="Weather Forecast App (React Native + Expo)"
+                    width={3000}
+                    height={2000}
+                    onClick={() =>
+                      router.push(
+                        "https://expo.dev/preview/update?message=Publishing+Weather+Application&updateRuntimeVersion=1.0.0&createdAt=2025-09-02T00%3A26%3A36.713Z&slug=exp&projectId=bf7b74cf-6294-437a-b78e-c98a17564095&group=79b26f1c-aa42-417c-95b3-12ace4a547cf"
+                      )
+                    }
+                  />
+                </motion.div>
+
+                <div className="w-full xl:w-1/2 mt-4 sm:mt-6 xl:mt-0">
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: title === "WEATHER_APP" ? 1 : 0 }}
+                    transition={{ duration: 1, ease: "linear" }}
+                  >
+                    {/* Title and Switch */}
+                    <div className="flex items-center justify-between">
+                      {/* title */}
+                      <h1 className="text-[14px] sm:text-[17px] md:text-[20px] xl:text-[22px]  font-semibold leading-[18px] md:leading-[30px]">
+                        Weather Forecast App (React Native + Expo)
+                      </h1>
+                      {/* dark/ light mode button */}
+                      <Switch
+                        checked={mode === "dark"}
+                        onChange={() => {
+                          setMode(mode === "light" ? "dark" : "light");
+                        }}
+                        className="group inline-flex h-4 w-8 sm:h-6 sm:w-11 items-center rounded-full bg-orange-500 transition data-[checked]:bg-green-600 hoverable"
+                      >
+                        <span className="size-3 sm:size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-4 sm:group-data-[checked]:translate-x-6 flex items-center justify-center">
+                          {mode === "light" ? (
+                            <LuSunMedium className="scale-75 text-orange-600 font-bold" />
+                          ) : (
+                            <FaRegMoon className="scale-75 text-green-600 font-bold" />
+                          )}
+                        </span>
+                      </Switch>
+                    </div>
+                    {/* period */}
+                    <h2 className="text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] mt-2 sm:mt-4 xl:mt-5 text-[#888888] dark:text-[#b4b4b4]">
+                      Jun 2025 - Aug 2025
+                    </h2>
+                    {/* Explanation */}
+                    <h3 className="mt-2 sm:mt-4 xl:mt-5 text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] leading-[15px] sm:leading-[30px]">
+                      {locale === "en"
+                        ? "A cross-platform weather app for iOS and Android, built with React Native and Expo. It provides current, hourly, and daily forecasts. The app automatically adjusts its theme according to local sunrise and sunset times, allows users to save their favorite cities, and view them on a map with detailed weather information."
+                        : "En tværplatforms-vejrapp til iOS og Android, bygget med React Native og Expo. Den giver aktuelle, time-for-time og daglige vejrudsigter. Appen justerer automatisk sit tema i henhold til lokal solopgang og solnedgang, og brugere kan gemme deres foretrukne byer og se dem på et kort med detaljerede vejrdata."}
+                    </h3>
+                  </motion.div>
+
+                  {/* skills */}
+                  <div className="flex gap-2 mt-4 sm:mt-6 flex-wrap">
+                    <motion.h1
+                      className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                    >
+                      HTML
+                    </motion.h1>
+                    <motion.h1
+                      className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                    >
+                      CSS
+                    </motion.h1>
+                    <motion.h1
+                      className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                    >
+                      NativeWind
+                    </motion.h1>
+                    <motion.h1
+                      className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                    >
+                      TypeScript
+                    </motion.h1>
+                    <motion.h1
+                      className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                    >
+                      React Native
+                    </motion.h1>
+                    <motion.h1
+                      className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                    >
+                      Expo
+                    </motion.h1>
+                    <motion.h1
+                      className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                    >
+                      Github
+                    </motion.h1>
+                  </div>
+
+                  {/* buttons */}
+                  <div className="flex justify-between mt-6 sm:mt-11 gap-10">
+                    <motion.button
+                      className="bg-none w-1/2 py-3 text-orange-500 font-medium border-2 border-solid border-orange-500 hover:no-underline hoverable hover:bg-orange-500 hover:text-white hover:dark:text-white dark:border-green-500 dark:text-green-500 hover:dark:bg-green-500 text-[13px] sm:text-base"
+                      onClick={() =>
+                        router.push(
+                          "https://expo.dev/preview/update?message=Publishing+Weather+Application&updateRuntimeVersion=1.0.0&createdAt=2025-09-02T00%3A26%3A36.713Z&slug=exp&projectId=bf7b74cf-6294-437a-b78e-c98a17564095&group=79b26f1c-aa42-417c-95b3-12ace4a547cf"
+                        )
+                      }
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                      whileHover={{
+                        scale: 1.05,
+                        transition: { duration: 0.2, ease: "easeInOut" },
+                      }}
+                    >
+                      Move to page
+                    </motion.button>
+                    <motion.button
+                      className="bg-none w-1/2 py-3 text-orange-500 font-medium border-2 border-solid border-orange-500 hover:no-underline hoverable hover:bg-orange-500 hover:text-white hover:dark:text-white dark:border-green-500 dark:text-green-500 hover:dark:bg-green-500 text-[13px] sm:text-base"
+                      onClick={() =>
+                        window.open(
+                          "https://github.com/junbeomwooo/react-native-weather-app"
+                        )
+                      }
+                      initial={{ scale: 0 }}
+                      animate={{ scale: title === "WEATHER_APP" ? 1 : 0 }}
+                      transition={{ duration: 0.5, ease: "easeInOut" }}
+                      whileHover={{
+                        scale: 1.05,
+                        transition: { duration: 0.2, ease: "easeInOut" },
+                      }}
+                    >
+                      View on Github
+                    </motion.button>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+
             {/* fotbold */}
             <SwiperSlide>
               <div className="w-full h-auto block xl:flex justify-between gap-[60px] py-4 xl:py-14 px-10 sm:px-20 items-center">
@@ -118,7 +289,7 @@ export default function Project() {
                     </div>
                     {/* period */}
                     <h2 className="text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] mt-2 sm:mt-4 xl:mt-5 text-[#888888] dark:text-[#b4b4b4]">
-                      Sep 2024 - Present
+                      Sep 2024 - May 2025
                     </h2>
                     {/* Explanation */}
                     <h3 className="mt-2 sm:mt-4 xl:mt-5 text-[11px] sm:text-[13px] md:text-[16px] xl:text-[18px] leading-[15px] sm:leading-[30px]">
@@ -160,7 +331,7 @@ export default function Project() {
                       animate={{ scale: title === "FOTBOLD" ? 1 : 0 }}
                       transition={{ duration: 0.5, ease: "easeInOut" }}
                     >
-                      Next.js
+                      TypeScript
                     </motion.h1>
                     <motion.h1
                       className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
@@ -168,7 +339,7 @@ export default function Project() {
                       animate={{ scale: title === "FOTBOLD" ? 1 : 0 }}
                       transition={{ duration: 0.5, ease: "easeInOut" }}
                     >
-                      TypeScript
+                      Next.js
                     </motion.h1>
                     <motion.h1
                       className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
@@ -491,7 +662,7 @@ export default function Project() {
                       animate={{ scale: title === "NMP" ? 1 : 0 }}
                       transition={{ duration: 0.5, ease: "easeInOut" }}
                     >
-                      Next.js
+                      TypeScript
                     </motion.h1>
                     <motion.h1
                       className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
@@ -499,7 +670,7 @@ export default function Project() {
                       animate={{ scale: title === "NMP" ? 1 : 0 }}
                       transition={{ duration: 0.5, ease: "easeInOut" }}
                     >
-                      TypeScript
+                      Next.js
                     </motion.h1>
                     <motion.h1
                       className="px-1 sm:px-3 py-0.5 sm:py-1 bg-orange-500 dark:bg-green-600 text-[10px] sm:text-sm rounded-lg text-white font-semibold"
